3bb12059ef1fb277ca54e8cec30f54401b8f9c7a
Jason Reading <jason.reading@sonyatv.com>
2018-06-30 21:14:15 +0100

    Test create

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Todo;
+use App\Form\TodoType;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController as Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
 
 class TodoController extends Controller
 {
@@ -15,7 +17,47 @@ class TodoController extends Controller
      */
     public function list()
     {
-        $view = $this->view($this->getDoctrine()->getRepository(Todo::class)->findAll(), 200);
-        return $this->handleView($view);
+        return $this->getDoctrine()->getRepository(Todo::class)->findAll();
+    }
+
+    /**
+     * @Rest\Post("/api/todo", name="todo.create")
+     * @View(serializerGroups={"detail"}, statusCode=201)
+     * @param Request $request
+     * @return array|mixed
+     */
+    public function create(Request $request)
+    {
+        return $this->edit($request);
+    }
+
+    /**
+     * @Rest\Post("/api/todo/{id}", name="todo.edit")
+     * @View(serializerGroups={"detail"}, statusCode=200)
+     * @param Request   $request
+     * @param Todo|null $todo
+     * @return array|mixed
+     */
+    public function edit(Request $request, Todo $todo = null)
+    {
+        $form = $this->get('form.factory')->create(TodoType::class, $todo);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todo = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+            return $todo;
+        }
+
+        return $this->view($form, 400);
+    }
+
+    public function delete(Todo $todo)
+    {
+
+
     }
 }
--- tests/Controller/TodoControllerTest.php
+++ tests/Controller/TodoControllerTest.php
@@ -10,18 +10,41 @@ class TodoControllerTest extends WebTestCase
 {
     /** @var \Doctrine\Common\DataFixtures\ReferenceRepository */
     private $fixtures;
-    public function setup(){
+
+    public function setup()
+    {
         $this->fixtures = $this->loadFixtures([LoadTodoFixtures::class])->getReferenceRepository();
     }
 
-    public function testSomething()
+    public function testList()
     {
         $client = static::createClient();
-
-        // Add some fixture data
         $client->request('GET', '/api/todo', ['Accept' => 'application/json']);
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $data = \json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals([
+            [
+                'id' => 1,
+                'description' => 'Hello World',
+                'completed' => false,
+                'created_date' => '2018-01-01T00:00:00+00:00',
+            ],
+            [
+                'id' => 2,
+                'description' => 'Goodbye, old friend',
+                'completed' => true,
+                'created_date' => '2018-02-01T00:00:00+00:00',
+            ],
+        ], $data);
+    }
+
+    public function testCreate()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/api/todo', ['todo' => ['description' => 'Testing']]);
+        $this->assertContains('"description":"Testing"', $client->getResponse()->getContent());
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
     }
 }
