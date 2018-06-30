dab0c134b5a8e3379f048bac4be05c9f0fced984
Jason Reading <jason.reading@sonyatv.com>
2018-06-30 18:47:12 +0100

    Install PHPUnit, start writing TodoTypeTest

---
+++ src/Form/TodoType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Todo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class TodoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', TextType::class, [
+                'required' => true,
+                'constraints' => [new NotNull(), new Length(['max' => 255])],
+            ])
+            ->add('completed', CheckboxType::class, [
+                'required' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Todo::class,
+        ]);
+    }
+}
